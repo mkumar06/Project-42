58388bb29f1ae4dd01ef198150cbc92f6f282934
Monish <s.monish2006@gmail.com>
2021-06-21 18:16:43 -0400

    started Project 42

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Calculating time using predefined functions from p5.js
+var hr = hour();
+var mn = minute();
+var sc = second();
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  angleMode(DEGREES);
+
+  hrAngle = map(hr, 0, 60%12, 0, 360)
+  mnAngle = map(mn, 0, 60, 0, 360)
+  scAngle = map(sc, 0, 60, 0, 360)
+
+  //drawing seconds hand
+  push();
+  rotate(scAngle); //rotate the hand based on angle calculated
+  stroke(255, 0, 0)
+  strokeWeight(7);
+  line(0, 0, 100, 0)
+  pop();
+
+  drawSprites();
+}
